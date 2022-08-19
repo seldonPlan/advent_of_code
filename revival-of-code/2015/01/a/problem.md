20d2affa35b53956772a183a20bea9105251e9a0
Jonas W. Carson <jonas.w.carson@disney.com>
2022-08-19 13:00:59 -0400

    finish [revival] 2015/1/a

--- revival-of-code/2015/01/a/problem.md
+++ revival-of-code/2015/01/a/problem.md
@@ -1,3 +1,5 @@
+[adventofcode.com/2015/day/1](https://adventofcode.com/2015/day/1)
+
 ## \--- Day 1: Not Quite Lisp ---
 
 Santa was hoping for a white Christmas, but his weather machine's "snow" function is powered by stars, and he's fresh out! To save Christmas, he needs you to collect _fifty stars_ by December 25th.
--- revival-of-code/2015/01/a/solution.py
+++ revival-of-code/2015/01/a/solution.py
@@ -1,19 +1,32 @@
-from typing import Any, List
-
-
-def parse_input(filename: str = "input.txt") -> List[Any]:
-    rv: List[Any] = []
+def parse_input(filename: str = "input.txt") -> tuple[str, int, int]:
+    rv: str = ""
+    up: int = 0
+    down: int = 0
     with open(filename, mode="rt") as inputfile:
-        for line in inputfile:
-            rv.append(line.strip())
+        while True:
+            s = inputfile.read(1)
+            rv += s
+            match s:
+                case "(":
+                    up += 1
+                case ")":
+                    down += 1
+                case "":
+                    break
 
-    return rv
+    return rv, up, down
 
 
-input = parse_input()
+input, up, down = parse_input()
+
+print("up:", up)
+print("down:", down)
+print("up - down:", up - down)
 
 with open("result.txt", mode="wt") as result:
-    result.write(str(""))
+    result.write(f"{up - down}\n")
 
 with open("output.txt", mode="wt") as outfile:
-    outfile.write(str(""))
+    outfile.write(f"number of up directions [(]: {up}\n")
+    outfile.write(f"number of down direction [)]: {down}\n")
+    outfile.write(f"up [{up}] - down [{down}] = final floor [{up - down}]")
