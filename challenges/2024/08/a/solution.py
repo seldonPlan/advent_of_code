abb6302bc2abc5cbbb4b20259bc75434e3388b25
seldonplan <jonas0226@gmail.com>
2024-12-08 22:24:19 -0500

    add 2024/08 completed solutions

---
+++ challenges/2024/08/a/solution.py
@@ -0,0 +1,86 @@
+import itertools
+from pathlib import Path
+from typing import Any
+
+
+def parse_input(filename: str = "input.txt") -> list[Any]:
+    rv: list[Any] = []
+    with open(Path(__file__).parent.joinpath(filename), mode="rt") as inputfile:
+        for line in inputfile:
+            rv.append(list(line.strip().replace(".", " ")))
+
+    return rv
+
+
+def calculate_antinode(row1, col1, row2, col2):
+    _row = row1 + (2 * (row2 - row1))
+    _col = col1 + (2 * (col2 - col1))
+
+    return _row, _col
+
+
+def is_valid(row, col, data):
+    if row < 0 or col < 0:
+        return False
+
+    try:
+        data[row][col]
+    except IndexError:
+        return False
+
+    return True
+
+
+data = parse_input("input.txt")
+
+output = {}
+for row in range(len(data)):
+    for col in range(len(data[row])):
+        if data[row][col] != " ":
+            if f"id_{data[row][col]}" not in output:
+                output[f"id_{data[row][col]}"] = {
+                    "value": data[row][col],
+                    "coords": [],
+                }
+            output[f"id_{data[row][col]}"]["coords"].append((row, col))
+
+all_antinodes = set()
+for _k, v in output.items():
+    perms = list(itertools.permutations(v["coords"], 2))
+    v["perms"] = perms
+    antinodes = set()
+    for perm in perms:
+        candidate = calculate_antinode(*perm[0], *perm[1])
+        if is_valid(*candidate, data):
+            all_antinodes.add(candidate)
+            antinodes.add(candidate)
+
+    v["antinodes"] = list(antinodes)
+
+
+with open("output.txt", mode="wt") as outfile:
+    for row in data:
+        outfile.write("".join(row))
+        outfile.write("\n")
+
+with open("output.mapping.txt", mode="wt") as outfile:
+    for k, v in output.items():
+        perms = list(itertools.permutations(v["coords"], 2))
+        outfile.write(f'"{k}":\n')
+        outfile.write(f'\t"value": {v["value"]},\n')
+        outfile.write(f'\t"coords": {v["coords"]},\n')
+        outfile.write(f'\t"perms": {v["perms"]},\n')
+        outfile.write(f'\t"antinodes": {v["antinodes"]},\n')
+        outfile.write("\n")
+
+with open("output.antinodes.txt", mode="wt") as outfile:
+    for row in range(len(data)):
+        for col in range(len(data[row])):
+            if (row, col) in all_antinodes:
+                outfile.write("#")
+            else:
+                outfile.write(" ")
+        outfile.write("\n")
+
+with open("result.txt", mode="wt") as resultfile:
+    resultfile.write(str(len(all_antinodes)))
