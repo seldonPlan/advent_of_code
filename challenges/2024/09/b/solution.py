a84b18c46278ddb49a87b6b61b8290895436a7b2
seldonplan <jonas0226@gmail.com>
2024-12-09 15:59:17 -0500

    mod complete 2024/09/b

--- challenges/2024/09/b/solution.py
+++ challenges/2024/09/b/solution.py
@@ -1,6 +1,6 @@
 from collections import deque, namedtuple
 from pathlib import Path
-from typing import Any
+from typing import Any, Literal
 
 
 def parse_input(filename: str = "input.txt") -> list[Any]:
@@ -13,72 +13,122 @@ def parse_input(filename: str = "input.txt") -> list[Any]:
     return [int(v) for v in rv]
 
 
-def pairs(source: list[str]):
+def make_pairs(source: list[str]):
     for idx in range(0, len(source), 2):
         yield (source[idx], source[idx + 1])
 
 
 ItemId = namedtuple("ItemId", ["id", "size", "spacing"])
 
-data = parse_input("input.example.txt")
-
-# populate initial mapping of src queue
-#   - one ItemId for each size count
-#   - one "." for each spacing count
-src = deque()
-tgt = deque()
-spaces = deque()
-for idx, item in enumerate(list(pairs(data))):
-    _item = ItemId(idx, *item)
-
-    src.extend([_item for _i in range(_item.size)])
-    src.extend(["." for _i in range(_item.spacing)])
+data = parse_input("input.txt")
+# print(data, list(make_pairs(data)), sep="\n")
 
 
-def fetch_next(q):
-    _items = []
-    _spaces = []
+class ItemWalker:
+    def __init__(self, pairs: list[tuple[int, int]]):
+        self._data: list[ItemId | Literal["."]] = []
 
+        for idx, item in enumerate(pairs):
+            _item = ItemId(idx, *item)
+
+            self._data.extend([_item for _i in range(_item.size)])
+            self._data.extend(["." for _i in range(_item.spacing)])
+
+        self._src = tuple(self._data)
+        self.right_cursor = (len(self._data) - 1, max([item.id for item in self._data if item != "."]))
+        self.left_cursor = 0
+
+    @property
+    def src(self):
+        return self._src
+
+    @property
+    def tgt(self):
+        return tuple(self._data)
+
+    def walk(self):
+        while True:
+            item, item_coords = self.next_item_candidate()
+            space_coords = self.next_space_candidate(item.size)
+
+            if len(space_coords) == len(item_coords) and max(space_coords) < min(item_coords):
+                # print(
+                #     "candidate item ",
+                #     item,
+                #     " | right side item coords ",
+                #     item_coords,
+                #     " | left side space coords ",
+                #     space_coords,
+                #     sep="",
+                # )
+                self.swap(space_coords, item_coords)
+
+            if item.id == 0:
+                break
+
+    def next_item_candidate(self):
+        for idx in range(self.right_cursor[0], 0, -1):
+            candidate = self._data[idx]
+            if candidate != "." and candidate.id == self.right_cursor[1]:
+                self.right_cursor = (idx - candidate.size, self.right_cursor[1] - 1)
+                return candidate, tuple(sorted(range(idx, idx - candidate.size, -1)))
+
+    def next_space_candidate(self, size):
+        run = []
+        for idx in range(len(self._data)):
+            if self._data[idx] == ".":
+                run.append(idx)
+                if len(run) == size:
+                    return tuple(run)
+
+                continue
+
+            run = []
+
+        return ()
+
+    def swap(self, space_coords, item_coords):
+        for coords in zip(space_coords, item_coords, strict=True):
+            _c = self._data[coords[0]]
+            self._data[coords[0]] = self._data[coords[1]]
+            self._data[coords[1]] = _c
+
+
+iw = ItemWalker(list(make_pairs(data)))
+# iw.walk()
+# print("".join([str(v.id) if v != "." else v for v in iw.src]))
+# print("".join([str(v.id) if v != "." else v for v in iw.tgt]))
+
+with open("output.txt", mode="wt") as outfile:
     while True:
-        candidate = q.pop()
-        if candidate == ".":
-            _spaces.append(candidate)
-            continue
+        item, item_coords = iw.next_item_candidate()
+        space_coords = iw.next_space_candidate(item.size)
 
-        break
+        outfile.write(f"candidate item: {item} | ")
+        outfile.write(f"right side item coords: {item_coords} | ")
+        outfile.write(f"left side space coords: {space_coords}\n")
 
-    _items.append(candidate)
-    for _i in range(candidate.size - 1):
-        _checkme = q.pop()
-        assert _checkme == candidate
-        _items.append(_checkme)
+        if len(space_coords) == len(item_coords) and max(space_coords) < min(item_coords):
+            iw.swap(space_coords, item_coords)
 
-    return _items, _spaces
-
-
-print(fetch_next(src))
-print(fetch_next(src))
-print(fetch_next(src))
-print(fetch_next(src))
-print(fetch_next(src))
-print(fetch_next(src))
-print(fetch_next(src))
-print(fetch_next(src))
-print(fetch_next(src))
-print(fetch_next(src))
+        if item.id == 0:
+            break
 
 # calculate checksum using ItemId info, and actual positional indices in the target
 total = 0
-for idx, item in enumerate(tgt):
+for idx, item in enumerate(iw.tgt):
     if item == ".":
-        break
+        continue
 
     total += item.id * idx
 
-# with open("output.txt", mode="wt") as outfile:
-#     for idx, v in enumerate(list(tgt)):
-#         outfile.write(f"{idx} {v}\n")
-#     # outfile.write(f"len(tgt) {len(tgt)}\n")
+with open("output.src.txt", mode="wt") as outfile:
+    outfile.write("".join([str(v.id) if v != "." else v for v in iw.src]))
+    outfile.write("\n")
 
-# with open("result.txt", mode="wt") as resultfile:
-#     resultfile.write(str(total))
+with open("output.tgt.txt", mode="wt") as outfile:
+    outfile.write("".join([str(v.id) if v != "." else v for v in iw.tgt]))
+    outfile.write("\n")
+
+with open("result.txt", mode="wt") as resultfile:
+    resultfile.write(str(total))
